920bc3dbfa1f877661919d4393e6c2f27d8f83c5
MasterMoritz <MasterMoritz@users.noreply.github.com>
2019-03-16 16:09:56 +0100

    add component

---
+++ src/components/CreatePanel.vue
@@ -0,0 +1,34 @@
+<template>
+<div class="adv-panel-content">
+    <div class="adv-panel-title">
+        <h3>{{title}}</h3>
+    </div>
+    <hr>
+    <div class="adv-panel-img"></div>
+    <hr>
+    <div class="adv-panel-text">
+        {{text}}
+    </div>
+    <hr>
+    <div class="adv-panel-choices">
+        {{choices}}
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: "CreatePanel",
+    props: {
+        title:String,
+        text:String,
+        choices:String
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
+
+
---
+++ src/views/Create.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <CreatePanel title="MyTestAdventure" text="My Test text <br> \n Trying some stuff out." choices="placeholder"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import CreatePanel from "@/components/CreatePanel.vue";
+
+export default {
+  name: "create",
+  components: {
+    CreatePanel
+  }
+};
+</script>
