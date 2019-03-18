2d00673906260293027ee7d7d410092f8435bc5c
Carina Rogl <carina.rogl@chatvisor.com>
2019-03-18 16:51:53 +0100

    styles u co

---
+++ src/components/OverviewComponent.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="overviewBody">
+    <h1>{{ msg }}</h1>
+    <h3>Available Adventures</h3>
+    <ul>
+      <li>
+        <p>Adventure 1</p>
+      </li>
+      <li>
+        <p>Adventure 2</p>
+      </li>
+      <li>
+        <p>Adventure 3</p>
+      </li>
+    </ul>
+    
+
+    
+</div>
+</template>
+
+<script>
+export default {
+  name: "List",
+  props: {
+    msg: String
+  },
+  };
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #0ed5f2;
+}
+</style>
+
--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import styles from './assets/css/style.css';
+import './style.css';
 import "./registerServiceWorker";
 
 Vue.config.productionTip = false;
@@ -10,5 +10,15 @@ Vue.config.productionTip = false;
 new Vue({
   router,
   store,
+  
+
+  //el: '#app-4',
+  data: {
+    todos: [
+      { text: 'Learn JavaScript' },
+      { text: 'Learn Vue' },
+      { text: 'Build something awesome' }
+    ]
+  },
   render: h => h(App)
 }).$mount("#app");
