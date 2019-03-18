a8a232a8e82f4b55147ff75e31a8afb14ab4991a
MasterMoritz <MasterMoritz@users.noreply.github.com>
2019-03-19 00:13:05 +0100

    add apollo client integration

--- src/main.js
+++ src/main.js
@@ -5,13 +5,25 @@ import store from "./store";
 import styles from './assets/css/style.css';
 import "./registerServiceWorker";
 import Notifications from 'vue-notification';
+import ApolloClient from 'apollo-boost'
+import VueApollo from 'vue-apollo'
 
 Vue.config.productionTip = false;
 Vue.use(Notifications);
+Vue.use(VueApollo)
+
+const apolloClient = new ApolloClient({
+  // You should use an absolute URL here
+  //uri: 'https://api.graphcms.com/simple/v1/awesomeTalksClone'
+})
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+})
 
 new Vue({
   router,
   store,
+  apolloProvider,
   render: h => h(App)
 }).$mount("#app");
 
