739f170ba9982be0c688325acdc2cbdb10b34e35
MasterMoritz <MasterMoritz@users.noreply.github.com>
2019-03-19 01:09:41 +0100

    add vuetify

--- src/main.js
+++ src/main.js
@@ -5,12 +5,15 @@ import store from "./store";
 import './style.css';
 import "./registerServiceWorker";
 import Notifications from 'vue-notification';
-import ApolloClient from 'apollo-boost'
-import VueApollo from 'vue-apollo'
+import ApolloClient from 'apollo-boost';
+import VueApollo from 'vue-apollo';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
 
 Vue.config.productionTip = false;
 Vue.use(Notifications);
-Vue.use(VueApollo)
+Vue.use(VueApollo);
+Vue.use(Vuetify);
 
 const apolloClient = new ApolloClient({
   // You should use an absolute URL here
