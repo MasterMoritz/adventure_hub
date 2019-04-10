eb89a972a1f3879ad0262119bc58632733ea9ae2
MasterMoritz <MasterMoritz@users.noreply.github.com>
2019-04-10 23:39:51 +0200

    add create adventure functionality

--- src/components/NewAdventureForm.vue
+++ src/components/NewAdventureForm.vue
@@ -1,18 +1,20 @@
 <template>
   <v-form v-on:submit.prevent="console.log(form)">
+    <v-alert
+      :value="error"
+      type="error"
+      transition="scale-transition"
+    >Oops, something went wrong. Please try again.</v-alert>
     <v-container>
       <v-layout row justify-center>
         <v-flex xs12 sm6 md4>
-          <v-text-field label="Title" v-model="title">
-
-          </v-text-field>
-          
+          <v-text-field label="Title" v-model="title"></v-text-field>
         </v-flex>
       </v-layout>
       <v-layout row justify-center>
         <v-flex xs1 align-center>
-          <v-btn>
-              Create
+          <v-btn icon flat color="indigo accent-3" :loading="loading" @click="createAdventure">
+            <v-icon large>check_circle_outline</v-icon>
           </v-btn>
         </v-flex>
       </v-layout>
@@ -21,12 +23,50 @@
 </template>
 
 <script>
+import gql from "graphql-tag";
+
 export default {
   name: "NewAdventureForm",
   data() {
     return {
-      title: ""
+      title: "",
+      loading: false,
+      error: false
     };
+  },
+  methods: {
+    async createAdventure() {
+      this.loading = true;
+      try {
+        const result = await this.$apollo.mutate({
+          // Query
+          mutation: gql`
+            mutation($name: String!) {
+              insert_adventure(objects: { name: $name }) {
+                returning {
+                  adventureKey
+                }
+              }
+            }
+          `,
+          // Parameters
+          variables: {
+            name: this.title
+          }
+        });
+
+        // Result
+        this.error = true;
+        alert(
+          "i got clicked! new key = " +
+            result.data["insert_adventure"]["returning"][0]["adventureKey"]
+        );
+      } catch (err) {
+        this.error = true;
+      }
+
+      this.loading = false;
+    }
   }
 };
 </script>
